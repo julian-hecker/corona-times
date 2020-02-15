4b3ee5f768de9408aff8dbe176410040b4f6a08a
mnai01 <imatlak03@gmail.com>
2020-02-15 02:33:32 -0500

    Layout for Subscribe form is set up

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -7,10 +7,15 @@ import {
   DropdownMenu,
   DropdownItem
 } from "reactstrap";
+import { Form, Input } from "reactstrap";
+import axios from "axios";
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const dburl = "http://www.ianmatlak.com:8443/country.php";
 
 // const is required
-// this is destructoring handleinput
-const Subscribe = props => {
+// this is destructoring handleInput
+const Subscribe = () => {
   const [email, setEmail] = useState("");
   const [country, setCountry] = useState("");
 
@@ -23,10 +28,22 @@ const Subscribe = props => {
 
   const toggle = () => setDropdownOpen(prevState => !prevState);
 
+  // No 'Access-Control-Allow-Origin' header is present on the requested ERROR
+  const countryList = () => {
+    axios(proxyurl + dburl)
+      .then(({ data }) => {
+        let countrylist1 = data;
+        console.log(countrylist1);
+      })
+      .catch(() =>
+        console.log("Can’t access " + dburl + " response. Blocked by browser?")
+      );
+  };
+
   return (
-    <form className="subscribe-wrap">
+    <Form className="subscribe-wrap">
       <label>
-        <input
+        <Input
           type="text"
           placeholder="Email Address"
           email="email"
@@ -35,24 +52,29 @@ const Subscribe = props => {
         />
       </label>
       <label>
-        <input
-          type="text"
-          placeholder="Select Country"
-          country="country"
-          onChange={e => setCountry(e.target.value)}
-          value={country}
-        />
+        <Input
+          type="select"
+          bsSize="md"
+          onClick={e => setCountry(e.target.value)}
+        >
+          <option>China</option>
+          <option>USA</option>
+          <option>Japan</option>
+        </Input>
       </label>
-      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      {/* <Dropdown isOpen={dropdownOpen} toggle={toggle}>
         <DropdownToggle caret>Dropdown</DropdownToggle>
         <DropdownMenu>
           <DropdownItem>China</DropdownItem>
           <DropdownItem>United States</DropdownItem>
           <DropdownItem>Japans</DropdownItem>
         </DropdownMenu>
-      </Dropdown>
-      <Button>Test</Button>
-    </form>
+      </Dropdown>*/}
+      {/*Only get activated when clicking the drop down menu*/}
+      <Button type="button" className="btn btn-warning" onClick={countryList}>
+        Get Notified!
+      </Button>
+    </Form>
   );
 };
 
