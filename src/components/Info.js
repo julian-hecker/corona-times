cef69307a809417b17e6725b0b32beba8d74bc26
julian-hecker <julianheckerdev@gmail.com>
2020-02-15 22:09:25 -0500

    add info page

---
+++ src/components/Info.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Info.scss';
+
+function Info() {
+
+    return (
+        <article>
+            <em>If for whatever reason you feel you may be experiencing any kind of emergency.  Please contact your nearest emergency services.</em>
+
+            <h2>Coronavirus Symptoms:</h2>
+            Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe diseases such as <a href="https://www.who.int/emergencies/mers-cov/en/">Middle East Respiratory Syndrome (MERS-CoV)</a> and <a href="https://www.who.int/csr/sars/en/">Severe Acute Respiratory Syndrome (SARS-CoV). A novel coronavirus (nCoV)</a> is a new strain that has not been previously identified in humans.
+            Coronaviruses are zoonotic, meaning they are transmitted between animals and people.  Detailed investigations found that SARS-CoV was transmitted from civet cats to humans and MERS-CoV from dromedary camels to humans. Several known coronaviruses are circulating in animals that have not yet infected humans.
+            Common signs of infection include respiratory symptoms, fever, cough, shortness of breath and breathing difficulties. In more severe cases, infection can cause pneumonia, severe acute respiratory syndrome, kidney failure and even death.
+            Standard recommendations to prevent infection spread include regular hand washing, covering mouth and nose when coughing and sneezing, thoroughly cooking meat and eggs. Avoid close contact with anyone showing symptoms of respiratory illness such as coughing and sneezing.
+            (Source: <a href="https://www.who.int/health-topics/coronavirus">WHO.IT</a> -World Health Organization)
+            <h2>About us</h2>
+            Thanks for checking out our website.  We appreciate your time at, and we had fun at this year’s CEWIT Hackathon!
+            -Ian Matlack, Julian Hecker, Philip Guerrero
+        </article>
+
+
+    );
+}
+
+export default Info;
