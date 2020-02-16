70ab27972faec190ec94e815bbfa65f97a8ad3e8
julian-hecker <julianheckerdev@gmail.com>
2020-02-16 02:48:53 -0500

    first blood (deploy)

---
+++ build/precache-manifest.cb12e36a2a2412ba78c16314c1dd1dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842902f98cd9fefe052f2af66f0f6aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40fdc8e0e13ce067f82c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "30672cf0db1db29a29cb",
+    "url": "/static/css/main.48628501.chunk.css"
+  },
+  {
+    "revision": "40fdc8e0e13ce067f82c",
+    "url": "/static/js/2.e4f47f1a.chunk.js"
+  },
+  {
+    "revision": "5f2dfc729cbc5e03eb64677a2da4dad4",
+    "url": "/static/js/2.e4f47f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30672cf0db1db29a29cb",
+    "url": "/static/js/main.a0856227.chunk.js"
+  },
+  {
+    "revision": "d9c4fa9f68c3eedb03fa",
+    "url": "/static/js/runtime-main.241c0fb1.js"
+  }
+]);
