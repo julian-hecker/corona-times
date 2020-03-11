04e99f74c9c6aa369460d8dd1bbb4968b9c4dce4
mnai01 <imatlak03@gmail.com>
2020-03-11 11:20:29 -0400

    Added Table using new API

---
+++ src/components/DetailedTable/DetailedTable.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Table } from "reactstrap";
+import "./DetailedTable.scss";
+import uuid from "uuid/v4";
+
+class DetailedTable extends React.Component {
+  render() {
+    const stats = this.props.stats;
+    if (stats) {
+      return (
+        <div className="table-wrapper">
+          <Table dark>
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Total Cases</th>
+                <th>New Cases</th>
+                <th>Total Deaths</th>
+                <th>New Deaths</th>
+                <th>Active Cases</th>
+                <th>Serious Cases</th>
+                <th>Serious Condition</th>
+                <th>Total Cases / 1Mil Pop</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stats.map(item => {
+                return (
+                  <tr key={uuid()}>
+                    <th scope="row">{item.Country}</th>
+                    <td>{item.Total_Cases}</td>
+                    <td>{item.New_Cases}</td>
+                    <td>{item.Total_Deaths}</td>
+                    <td>{item.New_Deaths}</td>
+                    <td>{item.Total_Recovered}</td>
+                    <td>{item.Active_Cases}</td>
+                    <td>{item.Serious_Critical}</td>
+                    <td>{item.TotCasesOf1MilPop}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default DetailedTable;
