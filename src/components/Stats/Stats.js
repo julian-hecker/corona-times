490c933ea19e95544e644a203974482a84701f72
mnai01 <imatlak03@gmail.com>
2020-03-11 11:22:51 -0400

    Changed Folder Layout

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Stats.scss";
+
+class Stats extends React.Component {
+  render() {
+    const stats = this.props.stats[0];
+    if (stats) {
+      return (
+        <div className="Stats-wrapper">
+          <div className="box">
+            <h1>{stats["infected"]}</h1>
+            <h5>Total Infected</h5>
+          </div>
+          <div className="box">
+            <h1>{stats["dead"]}</h1>
+            <h5>Total Dead</h5>
+          </div>
+          <div className="box">
+            <h1>{stats["cured"]}</h1>
+            <h5>Total Cured</h5>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Stats;
